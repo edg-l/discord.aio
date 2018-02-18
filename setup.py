6ca394ec44e992b237e2fd656a00904ade6f3605
Edgar <git@edgarluque.com>
2018-02-18 21:26:35 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pydiscord',
+      version='0.1.0',
+      description='Discord API wrapper',
+      url='https://github.com/Ryozuki/pydiscord',
+      author='Edgar Luque',
+      author_email='contact@ryobyte.com',
+      license='MIT',
+      packages=['pydiscord'],
+      zip_safe=False)

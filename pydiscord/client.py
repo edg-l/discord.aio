3d3fefe661b9f7ce13c1e34f0999a8b3e6ad9169
Edgar <git@edgarluque.com>
2018-02-21 21:51:27 +0100

    move links to constants.py

--- pydiscord/client.py
+++ pydiscord/client.py
@@ -7,6 +7,7 @@ from .user import User
 from .guild import Guild, GuildMember
 from .base import DiscordObject
 from .version import VERSION_STR
+from .constants import DISCORD_API_URL
 
 FORMAT = '%(asctime)-15s: %(message)s'
 logging.basicConfig(level=logging.DEBUG)
@@ -25,8 +26,6 @@ class HTTPHandler:
         self.token = token
         self.loop = asyncio.get_event_loop()
         self.headers = ''
-        self.discord_url = 'https://discordapp.com/api'
-        self.discord_cdn = 'https://cdn.discordapp.com'
         self.update_headers()
 
     def update_headers(self):
@@ -35,7 +34,7 @@ class HTTPHandler:
     async def request_url(self, url):
         while True:
             async with aiohttp.ClientSession(headers=self.headers) as session:
-                async with session.get(self.discord_url + url) as res:
+                async with session.get(DISCORD_API_URL + url) as res:
                     logger.debug(await res.text())
                     logger.debug(res.status)
                     if res.status == 429:
@@ -106,9 +105,6 @@ class DiscordBot:
         """Requests information about the current user."""
 
         return await self.get_user("@me")
-    
-    def get_user_avatar_url(self, user):
-        return self.httpHandler.discord_cdn + f'/avatars/{user.id}/{user.avatar}.png'
 
     async def get_guilds(self):
         """Returns the list of guilds that the user belongs to.

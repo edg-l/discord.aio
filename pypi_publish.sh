503811b3f5e44974b059983a101ee8fb830e6a1e
Ryozuki <edgar@ryobyte.com>
2018-02-23 22:51:52 +0100

    add some comments to publish script

--- pypi_publish.sh
+++ pypi_publish.sh
@@ -2,10 +2,19 @@
 
 # Script made to publish the package to https://pypi.org/project/discord.py/
 
+# Go to script directory
 cd `(dirname $0)`
+
+# Cleanup
 rm -rf dist/
 rm -rf *.aio.egg-info
+
+# Generate files
 python3 setup.py sdist
 python3 setup.py bdist
+
+# Sign
 gpg --detach-sign -a dist/*.tar.gz
+
+#Upload
 twine upload dist/*

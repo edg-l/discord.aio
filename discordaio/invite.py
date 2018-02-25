b78c402a4135ea0e1f263387f9a92457b49cd780
Edgar <git@edgarluque.com>
2018-02-25 18:50:31 +0100

    more docs

--- discordaio/invite.py
+++ discordaio/invite.py
@@ -5,7 +5,15 @@ from .user import User
 
 
 class Invite(DiscordObject):
-    """Represents a code that when used, adds a user to a guild."""
+    """Represents a code that when used, adds a user to a guild.
+
+    .. versionadded:: 0.2.0
+
+    Attributes:
+        code (:obj:`str`): the invite code (unique ID)
+        guild (:class:`.Guild`): the guild this invite is for
+        channel (:class:`.Channel`): the channel this invite is for
+    """
 
     def __init__(self, code="", guild=None, channel=None):
         self.code = code
@@ -22,6 +30,20 @@ class Invite(DiscordObject):
 
 
 class InviteMetadata(DiscordObject):
+    """Represents the invite metadata
+
+    .. versionadded:: 0.2.0
+
+    Attributes:
+        inviter (:class:`.A`): user object user who created the invite
+        uses (:obj:`int`): number of times this invite has been used
+        max_uses (:obj:`int`): max number of times this invite can be used
+        max_age (:obj:`int`): duration (in seconds) after which the invite expires
+        temporary (:obj:`bool`): whether this invite only grants temporary membership
+        created_at (:obj:`int`): timestamp when this invite was created
+        revoked (:obj:`bool`): whether this invite is revoked
+    """
+
     def __init__(self, inviter=None, uses=0, max_uses=0, max_age=0, temporary=False,
                  created_at=None, revoked=False):
         self.inviter = inviter

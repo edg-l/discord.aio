42c64d44c19616b0fe686e3010a50fa044c2baf2
Edgar <git@edgarluque.com>
2018-02-25 18:56:35 +0100

    more docs

--- discordaio/activity.py
+++ discordaio/activity.py
@@ -2,18 +2,47 @@ from .base import DiscordObject
 
 
 class ActivityParty(DiscordObject):
+    """Activity party
+
+    .. versionadded:: 0.2.0
+
+    Attributes:
+        id (:obj:`str`, optional): the id of the party
+        size (:obj:`list` of :obj:`int`): array of two integers (current_size, max_size), used to show the party's current and maximum size
+    """
+
     def __init__(self, id="", size=[]):
         self.id = id
         self.size = size
 
 
 class ActivityTimestamps(DiscordObject):
+    """Activity timestamps
+
+    .. versionadded:: 0.2.0
+
+    Attributes:
+        start (:obj:`int`, optional): unix time (in milliseconds) of when the activity started
+        end (:obj:`int`, optional): unix time (in milliseconds) of when the activity ends
+    """
+
     def __init__(self, start=0, end=0):
         self.start = start
         self.end = end
 
 
 class ActivityAssets(DiscordObject):
+    """Activity assets
+
+    .. versionadded:: 0.2.0
+
+    Attributes:
+        large_image (:obj:`str`, optional): the id for a large asset of the activity, usually a snowflake
+        large_text (:obj:`str`, optional): text displayed when hovering over the large image of the activity
+        small_image (:obj:`str`, optional): the id for a small asset of the activity, usually a snowflake
+        small_text (:obj:`str`, optional): text displayed when hovering over the small image of the activity
+    """
+
     def __init__(self, large_image="", large_text="", small_image="", small_text=""):
         self.large_image = large_image
         self.large_text = large_text
@@ -22,6 +51,22 @@ class ActivityAssets(DiscordObject):
 
 
 class Activity(DiscordObject):
+    """Represents a discord activity
+
+    .. versionadded:: 0.2.0
+
+    Attributes:
+        name (:obj:`str`): the activity's name
+        type (:obj:`int`): activity type
+        url (:obj:`str`, optional): stream url, is validated when type is 1
+        timestamps (:class:`.Timestamps`): object unix timestamps for start and/or end of the game
+        application_id (:obj:`int`, optional): application id for the game
+        details (:obj:`str`, optional): what the player is currently doing
+        state (:obj:`str`, optional): the user's current party status
+        party (:class:`.Party`): object information for the current party of the player
+        assets (:class:`.Assets`): object images for the presence and their hover texts
+    """
+
     def __init__(self, name="", type=0, url="", timestamps=None, application_id=0,
                  details="", state="", party=None, assets=None):
         self.name = name
